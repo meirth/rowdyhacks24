a24b02f3c231d72dacfa2d9df0461f70f1676f96
mercedesfa <mercedesfalejandro@gmail.com>
2024-02-24 13:55:57 -0600

    updated index and new page

--- script.js
+++ script.js
@@ -1,37 +1,8 @@
 
-<html lang="en">
-<head>  <meta charset="UTF-8"></meta>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-    <title>Ticket Selling Website</title>
-    <link rel="stylesheet" href="styles.css"></link> </head>
+const jenniferLopezImage = document.querySelector('img[alt="Jennifer Lopez"]');
 
-<body>
-    <div class="container">
-        <header>
-            <h1>TICKET - REX </h1>
-            <a href="nextpage.html"><img src="jloimage1.jpg" alt="Jennifer Lopez"></img></a>
-        </header>
-        <div class="performers">
-            <h2>Featured Performer</h2>
-            <div class="performer">
-                <img src="hloimage1.jpg" alt="Jennifer Lopez"></img>
-                <h3>Jennifer Lopez</h3>
-            </div>
-            <h2>Upcoming Performers</h2>
-            <div class="scrolling-wrapper">
-                <div class="performer">
-                    <img src="jloimage1.jpg" alt="Performer 1"></img>
-                    <h3>Performer 1</h3>
-                </div>
-                <div class="performer">
-                    <img src="jloimage1.jpg" alt="Performer 2"></img>
-                    <h3>Performer 2</h3>
-                </div>
 
-            </div>
-        </div>
-    </div>
+jenniferLopezImage.addEventListener('click', function() {
+    console.log('Jennifer Lopez image clicked!');
 
-    <script src="script.js"></script>
-</body>
-</html>
+});
