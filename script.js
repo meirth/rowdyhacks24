335e1d259a6276c56b238d9813a18fcb0da5dfe5
mercedesfa <mercedesfalejandro@gmail.com>
2024-02-24 13:50:41 -0600

    added files

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-<! DOCTYPE html>
 
-<html lang ="en">
-    <head>
-        <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-          <title> Ticket-Rex</title>
-          <link rel = "stylesheet" href="styles.css"></link>
-        </meta>
-     </head>
+<html lang="en">
+<head>  <meta charset="UTF-8"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    <title>Ticket Selling Website</title>
+    <link rel="stylesheet" href="styles.css"></link> </head>
 
 <body>
-    <div className="container">
-        <header> 
-            <h1> Ticket-Rex <h1> 
-        <a href="nextpage.html"><img sr=" " alt="Jennifer Lopez"></img></a>
+    <div class="container">
+        <header>
+            <h1>TICKET - REX </h1>
+            <a href="nextpage.html"><img src="jloimage1.jpg" alt="Jennifer Lopez"></img></a>
         </header>
+        <div class="performers">
+            <h2>Featured Performer</h2>
+            <div class="performer">
+                <img src="hloimage1.jpg" alt="Jennifer Lopez"></img>
+                <h3>Jennifer Lopez</h3>
+            </div>
+            <h2>Upcoming Performers</h2>
+            <div class="scrolling-wrapper">
+                <div class="performer">
+                    <img src="jloimage1.jpg" alt="Performer 1"></img>
+                    <h3>Performer 1</h3>
+                </div>
+                <div class="performer">
+                    <img src="jloimage1.jpg" alt="Performer 2"></img>
+                    <h3>Performer 2</h3>
+                </div>
 
-        <div classes = "performers">
-            <h2> Featured Performer </h2>
-
+            </div>
         </div>
     </div>
+
+    <script src="script.js"></script>
 </body>
 </html>
